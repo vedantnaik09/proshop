69d2294675de0e37834f6a2ae8083474f819f912
VedantPC <vedant.naik09@gmail.com>
2023-11-19 21:46:04 +0530

    commit viva

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,18 +1,23 @@
 import {Navbar, Nav, Container} from 'react-bootstrap'
 import {FaShoppingCart, FaUser} from 'react-icons/fa'
-
+import {LinkContainer} from 'react-router-bootstrap'
 const Header = () => {
   return (
     <header>
         <Navbar bg='dark' variant="dark" expand="md" collapseOnSelect>
             <Container>
-                <Navbar.Brand href="/">ProShop</Navbar.Brand>
+                <LinkContainer to="/">
+                <Navbar.Brand>ProShop</Navbar.Brand>
+                </LinkContainer>
                 <Navbar.Toggle aria-controls="basic-navbar-nav"></Navbar.Toggle>
                 <Navbar.Collapse id='basic-navbar-nav'>
                     <Nav className='ms-auto'>
+                    <LinkContainer to="/cart">
                         <Nav.Link href="/cart"><FaShoppingCart/>Cart</Nav.Link>
-                        <Nav.Link href="/cart"><FaUser/>Sign In</Nav.Link>
-
+                    </LinkContainer>
+                    <LinkContainer to="/login"> 
+                        <Nav.Link><FaUser/>Sign In</Nav.Link>
+                    </LinkContainer>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
---
+++ frontend/src/screens/ProductScreen.jsx
@@ -0,0 +1,17 @@
+import { Link, useParams } from "react-router-dom"
+import {Row, Col, Image, ListGroup, Card, Button} from 'react-bootstrap'
+import Rating from "../components/Rating"
+import products from '../products'
+
+
+const ProductScreen = () => {
+    const {id: productId} = useParams()
+    const product= products.find((p)=>p._id === productId)
+  return (
+    <>
+        <Link className="btn btn-light my-3" to="/">Go Back</Link>
+    </>
+  )
+}
+
+export default ProductScreen
